47e4ed6528b8c9fd8aaa2d0f1788d85bdcf93037
Absolute Dimension <48641794+adimension@users.noreply.github.com>
2020-12-30 19:28:46 +0530

    cc

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,18 +5,3 @@
 export const environment = {
   production: false
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
-
-
-/*
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://github.com/ngrx/platform
-*/
